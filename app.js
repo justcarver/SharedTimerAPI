9964e33fa900e3df922292ee3cf5a400757c05aa
Justin Carver <jccarver@gmail.com>
2015-07-16 02:37:27 -0400

    updated app.js to allow Ember-data to interact with the API if running on the same machine

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', api);
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  	res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
